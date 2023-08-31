093e976917ac549849dc66f3aaf2efeeddadf871
MirageM <imiragesz@gmail.com>
2023-08-30 22:13:09 -0400

    Time Complexity: O(n) Space Complexity: O(1)

--- countDigits.go
+++ countDigits.go
@@ -1,4 +1,8 @@
 // Leetcode Problem 2520: Count the Digits That Divide A Number
+// Time Comlexity: O(n) where n is the number of digits in the number
+// Space Complexity: O(1)
+package main
+
 func countDigits(num int) int {
 	temp := num
 	count := 0
