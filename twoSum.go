71efb3156fd99ced8c24549224bd782b94ae791f
MirageM <imiragesz@gmail.com>
2023-08-31 12:08:09 -0400

    package main

--- twoSum.go
+++ twoSum.go
@@ -1,3 +1,5 @@
+package main
+
 func twoSum(nums []int, target int) []int {
 	for i := 0; i < len(nums)-1; i++ {
 		for j := i + 1; j < len(nums); j++ {
@@ -8,4 +10,3 @@ func twoSum(nums []int, target int) []int {
 	}
 	return []int{}
 }
-    
