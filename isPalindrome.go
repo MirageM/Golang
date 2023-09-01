36f3a7ce1866fd78169fdf6d25009948d5d52511
MirageM <imiragesz@gmail.com>
2023-09-01 12:57:49 -0400

    if rune(string_x[i]) != ruen(string_x[n - 1 -i])

--- isPalindrome.go
+++ isPalindrome.go
@@ -1,3 +1,14 @@
-func isPalindrome(x int) bool {
+package main
 
+import "strconv"
+
+func isPalindrome(x int) bool {
+	string_x := strconv.Itoa(x)
+	n := len(string_x)
+	for i := 0; i < n/2; i++ {
+		if rune(string_x[i]) != rune(string_x[n-1-i]) {
+			return false
+		}
+	}
+	return true
 }
