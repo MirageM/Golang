6dd4362eb22c8aae49789addb849f71c0c3d1a73
MirageM <imiragesz@gmail.com>
2023-09-04 17:54:19 -0400

    res = append(res, root.Val)

--- inorderTraversal.go
+++ inorderTraversal.go
@@ -7,5 +7,19 @@
  * }
  */
 func inorderTraversal(root *TreeNode) []int {
-
+	res := make([]int, 0, 101)
+	if root != nil {
+		res = help(root, res)
+	}
+	return res
+}
+func help(root *TreeNode, res []int) []int {
+	if root.Left != nil {
+		res = help(root.Left, res)
+	}
+	res = append(res, root.Val)
+	if root.Right != nil {
+		res = help(root.Right, res)
+	}
+	return res
 }
