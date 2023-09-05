4da657afe6f00174a47a1f3d7e377120df147481
MirageM <imiragesz@gmail.com>
2023-09-05 17:39:55 -0400

    chars := make(map[rune]int)

--- isAnagram.go
+++ isAnagram.go
@@ -1,3 +1,18 @@
 func isAnagram(s string, t string) bool {
+	chars := make(map[rune]int)
 
+	for _, v := range s {
+		chars[v]++
+	}
+
+	for _, v := range t {
+		chars[v]--
+	}
+
+	for _, v := range chars {
+		if v != 0 {
+			return false
+		}
+	}
+	return true
 }
