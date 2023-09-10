a46ebb48c066c5948e51db1dec8728f1c5d175b9
MirageM <imiragesz@gmail.com>
2023-09-10 12:56:37 -0400

    numArray.prefixTable = make([]int, len(nums))

--- numArray.go
+++ numArray.go
@@ -1,11 +1,24 @@
 type NumArray struct {
+	size        int
+	prefixTable []int
 }
 
 func Constructor(nums []int) NumArray {
-
+	numArray := new(NumArray)
+	numArray.prefixTable = make([]int, len(nums))
+	numArray.prefixTable[0] = nums[0]
+	for k := 1; k < len(nums); k++ {
+		numArray.prefixTable[k] = numArray.prefixTable[k-1] + nums[k]
+	}
+	return *numArray
 }
 
 func (this *NumArray) SumRange(left int, right int) int {
+	if left == 0 {
+		return this.prefixTable[right]
+	} else {
+		return this.prefixTable[right] - this.prefixTable[left-1]
+	}
 
 }
 
