872550e07ec91739d45f252d9ffac66cea4abc2a
MirageM <imiragesz@gmail.com>
2023-09-09 23:52:29 -0400

    Time Complexity: O(n*m) Space Complexity: O(n*m)

--- islandPerimeter.go
+++ islandPerimeter.go
@@ -1,5 +1,6 @@
 // Leetcode Problem 463: Island Perimeter
-
+// Time Complexity: O(n*m) where n is the number of rows and m is the number of columns
+// Space Complexity: O(n*m) + O(1) = O(n*m) where n is the number of rows and m is the number of columns
 package main
 
 func islandPerimeter(grid [][]int) int {
