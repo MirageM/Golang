1203db3596f82ef793a674d50b16fb993c8a3069
MirageM <imiragesz@gmail.com>
2023-09-11 11:17:37 -0400

    answer[i] *= multiplier multiplier *= nums[i]

--- productExceptSelf.go
+++ productExceptSelf.go
@@ -1,3 +1,14 @@
 func productExceptSelf(nums []int) []int {
-
+	answer := make([]int, len(nums))
+	multiplier := 1
+	for i := 0; i < len(nums); i++ {
+		answer[i] = multiplier
+		multiplier *= nums[i]
+	}
+	multiplier = 1
+	for i := len(nums) - 1; i >= 0; i-- {
+		answer[i] *= multiplier
+		multiplier *= nums[i]
+	}
+	return answer
 }
