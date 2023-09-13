a192f8cdbc4488a3e9671b47cdfbd8140404bf38
MirageM <imiragesz@gmail.com>
2023-09-13 16:51:29 -0400

    if c, ok := ms[k]; !ok || c != v

--- findTheDifference.go
+++ findTheDifference.go
@@ -1,3 +1,18 @@
-func findTheDifference(s string, t string) byte {
+package main
 
+func findTheDifference(s string, t string) byte {
+	ms, mt := make(map[byte]int), make(map[byte]int)
+	for i := 0; i < len(s); i++ {
+		ms[s[i]]++
+	}
+	for i := 0; i < len(t); i++ {
+		mt[t[i]]++
+	}
+	for k, v := range mt {
+		if c, ok := ms[k]; !ok || c != v {
+			return k
+		}
+	}
+	var b byte
+	return b
 }
